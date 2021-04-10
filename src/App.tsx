dfee8b4279541d16dfe8bf8c09e7f2debd42b7a4
zzboncak <zachary.zboncak@gmail.com>
2021-04-10 09:34:49 -0500

    got rid of boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav-bar">
+        <header className="App-header">
+          <p>
+            Weekend Bulletin
+          </p>
+          <a
+            className="App-link"
+            href="https://www.faithecc.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Church Website
+          </a>
+        </header>
+      </nav>
     </div>
   );
 }
