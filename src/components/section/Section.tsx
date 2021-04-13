2ff4eed9ecd3f46b4313dc8a16d6c9b91f47e0bc
zzboncak <zachary.zboncak@gmail.com>
2021-04-13 14:27:56 -0500

    cleaned up comments

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -3,13 +3,6 @@ import { SectionProps } from "../../types";
 import { Subsection } from "../subsection/Subsection";
 import "./Section.css";
 
-/**
- * A Section should be collapsable
- * A Sections main title should always be visible
- * When the title is clicked, the rest of the sub-sections are expanded
- * A Section contains other elements of the service
- * Some of those (like songs) may also be collapsable
- */
 export const Section: React.FC<SectionProps> = ({
   title,
   subsections
