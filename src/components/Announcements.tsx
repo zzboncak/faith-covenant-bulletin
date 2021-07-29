bc11ff2588dae889ffb58553b86cf413059f93d5
zzboncak <zachary.zboncak@gmail.com>
2021-07-29 09:58:24 -0500

    added announcements

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -4,38 +4,32 @@ import { Subsection } from "./subsection/Subsection";
 
 const announcements = generateAnnouncements([
   <p key={1}>
-    <strong>Thank You:</strong> A big thank you to everyone who helped
-    with VBS. Thank you for cookies, prayers, and to all our
-    volunteers! We soared high with amazing kids! Please pray for them
-    as they remember what they learned and continue their faith
-    journeys.
+    <strong>Phase 4:</strong> Some exciting news! Phase 4 of our
+    building project is set to begin on August 2. This should be
+    approximately a two-week project and will be the update to the
+    sanctuary. In preparation for this work, the sanctuary will need
+    to be completely cleared out by August 1. If you can help move
+    chairs to Peterson Hall after the service today, it would be
+    greatly appreciated.
   </p>,
   <p key={2}>
-    <strong>Lake Day, Sunday, July 18:</strong> Have a fun afternoon
-    with friends from Faith Covenant at Barb and Gene Frost&#39;s lake
-    home, 1528 Holiday Dr, Sandwich, Illinois. Bring your own meat to
-    grill and a dish to share. The grill will be hot at 12:30 pm. Lawn
-    games, beach, pontoon, and speed boat rides will be available. If
-    you have any questions contact Gene Frost at{" "}
-    <a href="tel:630-347-8213">630-347-8213</a>.
-  </p>,
-  <p key={3}>
     <strong>
       Worship Service and Picnic Lunch, Lake Geneva, August 8:
     </strong>{" "}
-    Please join us for worship at Covenant Harbor. We will meet at
-    10:30 am on the ground floor of the Geneva Bay Center. Plan on
-    staying for fellowship following the service. You can pack your
-    own picnic or purchase a box lunch. Lunch must be purchased in
-    advance. More info on our website{" "}
+    Due to the retreat, there will be no Worship Service at Faith
+    Covenant Church on Sunday, August 8. Please join us for worship at
+    Covenant Harbor. We will meet at 10:30 am on the ground floor of
+    the Geneva Bay Center. Plan on staying for fellowship following
+    the service. You can pack your own picnic or purchase a box lunch
+    for $6.50. To purchase a lunch,{" "}
     <a
-      href="https://www.faithecc.org/all-church-retreat/"
+      href="https://www.faithecc.org/events/worship-service-and-picnic-lunch-lake-geneva-2/"
       target="_blank"
-      rel="noreferrer"
+      rel="noreferrer noopener"
     >
-      HERE
+      CLICK HERE
     </a>
-    !
+    . (Lunch must be purchased in advance.)
   </p>
 ]);
 
